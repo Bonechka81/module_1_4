1d24e0cb15472cd2b9b171b54cd059656d36ad4a
Bonechka81 <asanova81.06@mail.ru>
2024-09-09 11:42:10 +0300

    Add files via upload

--- module_1_4.py
+++ module_1_4.py
@@ -1,8 +1,9 @@
 my_string = input('все получится ')
+print(len(my_string))
 print(my_string.upper())
 print(my_string.lower())
 print(my_string.replace(' ', ''))
-first_char=my_string[0]
-last_char=my_string[-1]
+first_char = my_string[0]
+last_char = my_string[-1]
 print(first_char)
 print(last_char)
